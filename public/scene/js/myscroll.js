fd4c34052046d4e8f5747093843c6b9b7ac1f373
SimplyY <SimplyYu@163.com>
2015-10-28 22:24:03 +0800

    set time out to iscroll

--- public/scene/js/myscroll.js
+++ public/scene/js/myscroll.js
@@ -1,13 +1,16 @@
 function loaded() {
-    var myScroll = new IScroll('.s-scroller', {
-        mouseWheel: true,
-        scrollbars: true
-    });
 
-    var mycroll = new IScroll('.c-scroller', {
-        mouseWheel: true,
-        scrollbars: true
-    });
+    setTimeout(function () {
+        var myScroll = new IScroll('.s-scroller', {
+            mouseWheel: true,
+            scrollbars: true
+        });
+
+        var mycroll = new IScroll('.c-scroller', {
+            mouseWheel: true,
+            scrollbars: true
+        });
+    }, 200);
 }
 
 document.addEventListener('touchmove', function(e) {
