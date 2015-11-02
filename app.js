cf489b3f7207e34a7468d78c899431cfffda2003
SimplyY <SimplyYu@163.com>
2015-11-02 16:45:37 +0800

    add gzip and can

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var compress = require('compression');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var _ = require('lodash');
@@ -27,9 +28,11 @@ app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
+app.use(compress()); 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'),{
