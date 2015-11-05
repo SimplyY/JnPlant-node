a60fdb30a4fcc39d44ded69ac5bd41201a445eec
SimplyY <SimplyYu@163.com>
2015-11-05 21:11:39 +0800

    change openId type

--- models/user.js
+++ models/user.js
@@ -2,12 +2,12 @@
 var mongoose = require('mongoose');
 
 // 使用安卓端  open id 检验唯一性
-// 先查询当前登录的 User open id 在不在服务器数据库表里面
+// 先 get 方法查询当前登录的 User open id 在不在服务器数据库表里面
 // 如果在，使用 put 方法，去更新服务器数据库的name和imgUrl，并且将get得到的lovesId存储到本地数据库
 // 如果不在，使用 post 方法，在服务器数据库增加一条数据
 var userSchema = new mongoose.Schema({
     openId: {
-        type:Number,
+        type: String,
         require: true
     },
     name: {
