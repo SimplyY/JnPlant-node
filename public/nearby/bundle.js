abf02a214c81213607c5a30eecbc79e648801104
SimplyY <SimplyYu@163.com>
2016-01-16 21:33:58 +0800

    use andriod webToast

--- public/nearby/bundle.js
+++ public/nearby/bundle.js
@@ -1,5 +1,4 @@
 setInterval(function () {
     var location = andriod.getCurrentLocation();
-    var domTest = document.getElementById('test');
-    domTest.text = location.longitude.toString() + location.latitude.toString();
+    andriod.webToast(location.longitude.toString() + location.latitude.toString())
 }, 100);
