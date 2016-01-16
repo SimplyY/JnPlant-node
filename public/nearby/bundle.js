2513c8bf7fd25e24220b5bf495d1d6d14b72dee5
SimplyY <SimplyYu@163.com>
2016-01-16 21:14:21 +0800

    add timer to run android api

--- public/nearby/bundle.js
+++ public/nearby/bundle.js
@@ -1,3 +1,5 @@
-var location = andriod.getCurrentLocation();
-var domTest = document.getElementById('test');
-domTest.text = location.longitude.toString() + location.latitude.toString();
+setInterval(function () {
+    var location = andriod.getCurrentLocation();
+    var domTest = document.getElementById('test');
+    domTest.text = location.longitude.toString() + location.latitude.toString();
+}, 100);
