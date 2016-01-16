0d843ce8d8f0a18ec6a16d4d9af4161da204fab3
SimplyY <SimplyYu@163.com>
2016-01-16 21:37:29 +0800

    toast test

--- public/nearby/bundle.js
+++ public/nearby/bundle.js
@@ -1,4 +1,5 @@
 setInterval(function () {
     var location = andriod.getCurrentLocation();
+    andriod.webToast('test');
     andriod.webToast(location.longitude.toString() + location.latitude.toString())
 }, 100);
